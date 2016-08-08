a41ea0229dff168e95425196e1df5b748747f575
seamenondeck <this.mailing.address@gmail.com>
2016-08-08 10:48:07 +0800

    Add files via upload

---
+++ numbertoroman.c
@@ -0,0 +1,90 @@
+/**
+ *Program to convert Decimal Numbers into their roman Value.
+ *Date Created : 08/07/16:3:30.P.M
+ *Written By: Dean M. Angco
+ */
+#include <stdio.h>
+int main(){
+
+    //Variable Declaration
+    int number;//Variable to Store user input
+    int thousands;//Variable to Store number divided by 1000;
+    int hundreds;//Variable to Store number divided by 100;
+    int tens;//Variable to Store number divided by 10;
+    int ones;//Variable to Store number divided by 1;
+    int temp=0;//Variable for controlling the for loop
+
+    printf("Enter Number to Convert:");
+    scanf("%d",&number);//get user input
+
+    if(number>999 && number <4000){//if number is between 1000-4000 do the following block of code
+        thousands = number/1000; //divide the user input by 1000, here we can only get a maximum of 3
+        for(temp=1;temp<=thousands;temp++){//loop starting from 1 until reaches the value of variable 'thousand'
+            printf("M");//No additional condition for we'll only be getting numbers below 4k which is MMM in roman
+        }
+        number%=1000;//Modulo operation:gets the remainder then assign it to 'number' variable
+    }
+    if(number>99){//if number is between 99 - 1000 do the following block of code
+        hundreds = number/100;//divide the user input by 1000,
+        if(hundreds<=3){//if 'number' is < 4,such as 1,2,3 proceed to for loop
+            for(temp = 1;temp<=hundreds ;temp++){
+                printf("C");//100
+            }
+        }
+        if(hundreds==4){
+            printf("CD");//400
+        }
+        if(hundreds>=5 && hundreds < 9){
+            printf("D");//500
+            for(temp=0;temp<hundreds-5;temp++){
+                printf("C");
+            }
+        }
+        if(hundreds == 9){
+            printf("CM");
+        }
+
+        number%=100;
+    }
+    if(number>9){
+        tens = number/10;
+        if(tens<=3){
+            for(temp = 1;temp<=tens ;temp++){
+                printf("X" );//10,20,30
+            }
+        }
+        if(tens==4){
+            printf("XL");//40
+        }
+        if(tens>=5 && tens < 9){
+            printf("L");//50,60,70,80
+            for(temp=0;temp<tens-5;temp++){
+                printf("X");
+            }
+        }
+        if(tens == 9){
+            printf("XC");
+        }
+    number%=10;
+    }
+    if(number<10){
+    ones = number/1;
+        if(ones<=3){
+            for(temp = 1;temp<=ones ;temp++){
+                printf("I");//1,2,3
+            }
+        }
+        if(ones==4){
+            printf("IV");//4
+        }
+        if(ones>=5 && ones < 9){
+            printf("V");//5,6,7,8
+            for(temp=0;temp<ones-5;temp++){
+                printf("I");
+            }
+        }
+        if(ones == 9){
+            printf("IX");
+        }
+    }
+}
